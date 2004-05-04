caf01e28db4384fa554c58815a2d92c8b03e4a03
Eamon Walsh <ewalsh@epoch.ncsc.mil>
2004-05-04 19:44:11 +0000

    Merge the new release from HEAD

--- exec.c
+++ exec.c
@@ -1,4 +1,4 @@
-/* $XdotOrg: exec.c,v 1.4 2001/02/09 02:05:56 xorgcvs Exp $ */
+/* $XdotOrg: xc/programs/xmodmap/exec.c,v 1.2 2004/04/23 19:54:59 eich Exp $ */
 /* $Xorg: exec.c,v 1.4 2001/02/09 02:05:56 xorgcvs Exp $ */
 /*
 
